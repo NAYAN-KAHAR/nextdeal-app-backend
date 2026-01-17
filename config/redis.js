92ac3c029a77e97ab5ce9eed757247e9d0092841
NAYAN-KAHAR <kaharnayan99@gmail.com>
2026-01-17 11:22:58 +0530

    add redis

--- config/redis.js
+++ config/redis.js
@@ -3,14 +3,14 @@ dotenv.config();
 
 import { createClient } from 'redis';
 
-const redisClient =  createClient({
-   url: "redis://127.0.0.1:6379"
-});
-
-
-// const redisClient = createClient({
-//   url: process.env.REDIS_URL  // use the environment variable
+// const redisClient =  createClient({
+//    url: "redis://127.0.0.1:6379"
 // });
+
+
+const redisClient = createClient({
+  url: process.env.REDIS_URL  // use the environment variable
+});
 
 redisClient.on("error", (err) => {
   console.error("Redis connection error:", err);
